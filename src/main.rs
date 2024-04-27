dba295164cf1a192b23724516789d310a0b1e870
Ibis Sunrosa <drsunrosa@gmail.com>
2024-04-27 03:46:53 +0000

    Expects

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use serde::Serialize;
-
 mod model;
 
 fn main() {
     let test_score = std::fs::OpenOptions::new()
         .read(true)
         .open("test.json")
-        .unwrap();
+        .expect("Error reading test score from file");
 
-    let score: model::Score = serde_json::from_reader(test_score).unwrap();
-    println!("{}", serde_json::to_string(&score).unwrap());
+    let score: model::Score =
+        serde_json::from_reader(test_score).expect("Error deserializing test score");
+
+    println!(
+        "{}",
+        serde_json::to_string(&score).expect("Error serializing test score")
+    );
 }
